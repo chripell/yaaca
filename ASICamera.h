aaabb13138b8951e1f69229828c140ca47b96000
Christian Pellegrin <chripell@fsfe.org>
2014-01-18 20:16:29 +0100

    upgraded to latest ASICamera SDK and use static linking

--- ASICamera.h
+++ ASICamera.h
@@ -1,9 +1,25 @@
 
 #ifndef ASICAMERA_H
 #define ASICAMERA_H
-#define ASICAMERA_API 
 
-enum Control_TYPE{
+#ifdef _WINDOWS
+	#define ASICAMERA_API __declspec(dllexport)
+#elif defined _LIN
+	#define ASICAMERA_API 
+#endif
+
+enum CAMERA_TYPE{ //Camera Type
+	CAMERA_ASI120MM = 0,
+	CAMERA_ASI120MC,
+	CAMERA_ASI034MC,
+	CAMERA_ASI130MM,
+	CAMERA_ASI035MM,
+	CAMERA_ASI035MC,
+	CAMERA_ASI030MC,
+	CAMERA_UNKNOWN
+};
+
+enum Control_TYPE{ //Control ID
 	CONTROL_GAIN = 0,
 	CONTROL_EXPOSURE,
 	CONTROL_GAMMA,
@@ -21,7 +37,7 @@ enum IMG_TYPE{ //Supported image type
 	IMG_Y8,
 };
 
-enum GuideDirections{
+enum GuideDirections{ //Guider Direction
 	guideNorth=0,
 	guideSouth,
 	guideEast,
@@ -35,91 +51,98 @@ enum BayerPattern{
 	BayerGB
 };
 
-#ifndef _cplusplus
+#ifndef __cplusplus
 #define bool char
 #define BayerPattern int
+#define CAMERA_TYPE int
 #define Control_TYPE int
 #define IMG_TYPE int
 #define GuideDirections int
 #endif
 
-#ifdef _cplusplus
+#ifdef __cplusplus
 extern "C" {
 #endif
 
 // get number of Connected ASI cameras.
-int getNumberOfConnectedCameras(); 
-// Open  the  camera. camIndex 0 means the first one.
-bool openCamera(int camIndex);
+ASICAMERA_API  int getNumberOfConnectedCameras(); 
+//open Camera, camIndex 0 means the first one.
+ASICAMERA_API  bool openCamera(int camIndex);
+// init the  camera after Open
+ASICAMERA_API  bool initCamera();
 //don't forget to closeCamera if you opened one
-void closeCamera();
-// init the  camera. 
-bool initCamera();
+ASICAMERA_API  void closeCamera();
 //Is it a color camera?
-bool isColorCam();
+ASICAMERA_API  bool isColorCam();
 //get the pixel size of the camera
-double getPixelSize();
+ASICAMERA_API  double getPixelSize();
 // what is the bayer pattern
-BayerPattern getColorBayer();
+ASICAMERA_API  BayerPattern getColorBayer();
 //get the camera name. camIndex 0 means the first one.
-char* getCameraModel(int camIndex);
+ASICAMERA_API  char* getCameraModel(int camIndex);
+//get the camera menu type. camIndex 0 means the first one.
+ASICAMERA_API  CAMERA_TYPE getCameraType(int camIndex);
 
 
 // is control supported by current camera
-bool isAvailable(Control_TYPE control) ;   
+ASICAMERA_API bool isAvailable(Control_TYPE control) ;   
 // is control supported auto adjust
-bool isAutoSupported(Control_TYPE control) ;		
+ASICAMERA_API bool isAutoSupported(Control_TYPE control) ;		
 // get control current value and auto status
-int getValue(Control_TYPE control, bool *pbAuto)  ;    
+ASICAMERA_API int getValue(Control_TYPE control, bool *pbAuto)  ;    
 // get minimal value of control
-int getMin(Control_TYPE control) ;  
+ASICAMERA_API int getMin(Control_TYPE control) ;  
 // get maximal  value of control
-int getMax(Control_TYPE control) ;  
+ASICAMERA_API int getMax(Control_TYPE control) ;  
 // set current value and auto states of control
-void setValue(Control_TYPE control, int value, bool autoset); 
+ASICAMERA_API void setValue(Control_TYPE control, int value, bool autoset); 
+// set auto parameter
+ASICAMERA_API void setAutoPara(int iMaxGain, int iMaxExp, int iDestBrightness);
+// get auto parameter
+ASICAMERA_API void getAutoPara(int *pMaxGain, int *pMaxExp, int *pDestBrightness);
 
-int getMaxWidth();  // max image width
-int getMaxHeight(); // max image height
-int getWidth(); // get current width
-int getHeight(); // get current heigth
-int getStartX(); // get ROI start X
-int getStartY(); // get ROI start Y
+ASICAMERA_API  int getMaxWidth();  // max image width
+ASICAMERA_API  int getMaxHeight(); // max image height
+ASICAMERA_API  int getWidth(); // get current width
+ASICAMERA_API  int getHeight(); // get current heigth
+ASICAMERA_API  int getStartX(); // get ROI start X
+ASICAMERA_API  int getStartY(); // get ROI start Y
 
-float getSensorTemp(); //get the temp of sensor ,only ASI120 support
-unsigned long getDroppedFrames(); //get Dropped frames 
-bool SetMisc(bool bFlipRow, bool bFlipColumn);	//Flip x and y
-void GetMisc(bool * pbFlipRow, bool * pbFlipColumn); //Get Flip setting	
+ASICAMERA_API  float getSensorTemp(); //get the temp of sensor ,only ASI120 support
+ASICAMERA_API  unsigned long getDroppedFrames(); //get Dropped frames 
+ASICAMERA_API  bool SetMisc(bool bFlipRow, bool bFlipColumn);	//Flip x and y
+ASICAMERA_API  void GetMisc(bool * pbFlipRow, bool * pbFlipColumn); //Get Flip setting	
 
 //whether the camera support bin2 or bin3
-bool isBinSupported(int binning); 
+ASICAMERA_API  bool isBinSupported(int binning); 
 //whether the camera support this img_type
-bool isImgTypeSupported(IMG_TYPE img_type); 
+ASICAMERA_API  bool isImgTypeSupported(IMG_TYPE img_type); 
 //get the current binning method
-int getBin(); 
+ASICAMERA_API  int getBin(); 
 
 //call this function to change ROI area after setImageFormat
 //return true when success false when failed
-bool setStartPos(int startx, int starty); 
+ASICAMERA_API  bool setStartPos(int startx, int starty); 
 // set new image format - 
 //ASI120's data size must be times of 1024 which means width*height%1024=0
-bool setImageFormat(int width, int height,  int binning, IMG_TYPE img_type);  
+ASICAMERA_API  bool setImageFormat(int width, int height,  int binning, IMG_TYPE img_type);  
 //get the image type current set
-IMG_TYPE getImgType(); 
+ASICAMERA_API  IMG_TYPE getImgType(); 
 
 //start capture image
-void startCapture(); 
+ASICAMERA_API  void startCapture(); 
 //stop capture image
-void stopCapture();
+ASICAMERA_API  void stopCapture();
 
 
 // wait waitms capture a single frame -1 means wait forever, success return true, failed return false
-bool getImageData(unsigned char* buffer, int bufSize, int waitms);
+ASICAMERA_API bool getImageData(unsigned char* buffer, int bufSize, int waitms);
 
 //ST4 guide support. only the module with ST4 port support this
-void pulseGuide(GuideDirections direction, int timems);
+ASICAMERA_API void pulseGuide(GuideDirections direction, int timems);
 
-#ifdef _cplusplus
+#ifdef __cplusplus
 }
 #endif
-#endif
 
+#endif
--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 CC=gcc
-CFLAGS=$(shell pkg-config --cflags gtk+-2.0) -I. -g -O2 -Wall
-LDFLAGS=$(shell pkg-config --libs gtk+-2.0 libusb-1.0) -L./SDK/lib -lASICamera -lm -g
+CFLAGS=$(shell pkg-config --cflags gtk+-2.0) -I. -g -O2 -Wall -D_LIN
+LDFLAGS=$(shell pkg-config --libs gtk+-2.0 libusb-1.0) SDK/libASICamera.a -lstdc++ -lm -g
 
 yaaca: zwo.o yaaca.o
-	gcc $(CFLAGS) $(LDFLAGS) -o $@ $^
+	gcc $(CFLAGS) -o $@ $^ $(LDFLAGS)
 
 zwo.o: zwo.c yaaca.h
 
