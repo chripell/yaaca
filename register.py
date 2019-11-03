8ea80d548ce17bafa92fc6961a48fead9d92873e
Christian Pellegrin <chri@evolware.org>
2019-11-03 21:26:38 +0000

    Support flats with different shape.

--- register.py
+++ register.py
@@ -84,6 +84,38 @@ if dark != "N" :
 ref = None
 debug = False
 
+def bin_ndarray(ndarray, new_shape, operation='sum'):
+    """
+    Bins an ndarray in all axes based on the target shape, by summing or
+        averaging.
+    Number of output dimensions must match number of input dimensions.
+    Example
+    -------
+    >>> m = np.arange(0,100,1).reshape((10,10))
+    >>> n = bin_ndarray(m, new_shape=(5,5), operation='sum')
+    >>> print(n)
+    [[ 22  30  38  46  54]
+     [102 110 118 126 134]
+     [182 190 198 206 214]
+     [262 270 278 286 294]
+     [342 350 358 366 374]]
+    """
+    if not operation.lower() in ['sum', 'mean', 'average', 'avg']:
+        raise ValueError("Operation {} not supported.".format(operation))
+    if ndarray.ndim != len(new_shape):
+        raise ValueError("Shape mismatch: {} -> {}".format(ndarray.shape,
+                                                           new_shape))
+    compression_pairs = [(d, c//d) for d, c in zip(new_shape,
+                                                   ndarray.shape)]
+    flattened = [l for p in compression_pairs for l in p]
+    ndarray = ndarray.reshape(flattened)
+    for i in range(len(new_shape)):
+        if operation.lower() == "sum":
+            ndarray = ndarray.sum(-1*(i+1))
+        elif operation.lower() in ["mean", "average", "avg"]:
+            ndarray = ndarray.mean(-1*(i+1))
+    return ndarray
+
 def prev_pow(x):
     p = 1
     while p < x:
@@ -125,14 +157,22 @@ def prepare_image(n):
         imRGB = AL.demosaic(imRAW, debayer_pattern, debayer_method)
         imRGB = [x.astype(AL.myfloat) for x in imRGB]
     if not flat is None :
-        imRGB = [x / flat for x in imRGB]
+        if imRGB[0].shape != flat.shape:
+            fl = bin_ndarray(flat, imRGB[0].shape, operation='mean')
+            imRGB = [x / fl for x in imRGB]
+        else:
+            imRGB = [x / flat for x in imRGB]
     imRGB = [x[crop_x : (crop_x + crop_w), crop_y : (crop_y + crop_h)] for x in imRGB]
     if im_mode == 16:
         imSP1 = AL.demosaic(imRAW, debayer_pattern, AL.DEBAYER_SUPER_PIXEL)
         imSP = [np.repeat(np.repeat(x, 2, axis=0), 2, axis=1) for x in imSP1]
         imSP = [x.astype(AL.myfloat) for x in imSP]
         if not flat is None :
-            imSP = [x / flat for x in imSP]
+            if imSP[0].shape != flat.shape:
+                fl = bin_ndarray(flat, imSP[0].shape, operation='mean')
+                imSP = [x / fl for x in imSP]
+            else:
+                imSP = [x / flat for x in imSP]
         imSP = [x[crop_x : (crop_x + crop_w), crop_y : (crop_y + crop_h)] for x in imSP]
 
     if filter == 1 :
